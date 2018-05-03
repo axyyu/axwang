8674448e9cc65a075561b900138cc34f734e714e
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-05-02 23:35:59 -0400

    Redesign with new font

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
+var minify = require('gulp-minifier');
 
 gulp.task('default', function() {
-	gulp.watch('./sass/*.scss', ['sass']);
-	gulp.watch('./templates/*.pug', ['pug']);
+	gulp.watch('./dev/sass/*.scss', ['sass']);
+	gulp.watch('./dev/templates/*.pug', ['pug']);
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./dev/sass/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./w4v3s.github.io/css'));
 });
 
 gulp.task('pug', function () {
-  return gulp.src('./templates/*.pug')
+  return gulp.src('./dev/templates/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('./w4v3s.github.io/'));
 });
